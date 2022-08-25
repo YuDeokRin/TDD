2f27b6f204bb56225712d19addddf3163570a50b
YuDeokRin <ejrfls12@naver.com>
2022-08-25 13:01:29 +0900

    기존의 코드 리펙토링

--- test/java/chap02/PasswordStrengthMeter.java
+++ test/java/chap02/PasswordStrengthMeter.java
@@ -8,34 +8,27 @@ public class PasswordStrengthMeter {
         //5. null 값 또는 빈 문자열일 경우
         if(s == null || s.isEmpty()) return PasswordStrength.INVALID;
 
+        // WEAK(약함)을 반환하는 것들을 처리했다.
+        // 충족하는 조건을 개수로 바꾸면 묶어서 처리할 수 있다. (중복제거 )
+        
+        int metCounts = 0;
         boolean lengthEnough = s.length() >=8;
+        if(lengthEnough) metCounts++;
         boolean containsNum = meetsContainingNumberCriteria(s); // 0 ~ 9 라는 숫자를 포함하는지 안하는지 확인하는 변수
+        if(containsNum) metCounts++;
         boolean containsUpp = meetsContainingUppercaseCriteria(s);
+        if(containsUpp) metCounts++;
+
+        if(metCounts== 1) return PasswordStrength.WEAK; // 공통을 묶어줬따.
 
 
-        //6. 숫자 포함 x and 대문자 포함 x and 8글자 이상만 포함
-        if(lengthEnough && !containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        //7.
-        if(!lengthEnough && containsNum && !containsUpp)
-            return PasswordStrength.WEAK;
-        //8.
-        if(!lengthEnough && !containsNum && containsUpp)
-            return PasswordStrength.WEAK;
 
-        /* 이전 코드
-        // 1. 문자열의 길이가 8이하면
-        if (s.length() < 8) {
-            return PasswordStrength.NORMAL; // 패스워드의 강도는 보통이다.
-        }
-         */
+        //NORMAL(보통)을 반환하는 로직들을 묶음 처리
+        if(metCounts == 2) return PasswordStrength.NORMAL;
 
-        if(!lengthEnough) {
-            return PasswordStrength.NORMAL;
-        }
-        if(!containsNum) return PasswordStrength.NORMAL; // 각 문자를 비교해서 0 ~ 9 사이의 값을 갖는 문자가 없으면 NORMAL
-        if(!containsUpp) return PasswordStrength.NORMAL;
-        return PasswordStrength.STRONG; // 갖고 있으면 강함으로
+
+        //STRONG(강함)은 1도아니고 2도아니면 else 느낌으로 처리한다.
+        return PasswordStrength.STRONG;
     }
 
     //3. 숫자를 포함하지 않고 나머지 조건을 충족하는 경우
