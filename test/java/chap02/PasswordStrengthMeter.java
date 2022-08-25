159fdd70e972635b176da9ae31093d8b8650a60a
YuDeokRin <ejrfls12@naver.com>
2022-08-25 12:51:41 +0900

    8번째 테스트까지

--- test/java/chap02/PasswordStrengthMeter.java
+++ test/java/chap02/PasswordStrengthMeter.java
@@ -4,18 +4,55 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String s) {
-        if (s.length() < 8) { // 문자열의 길이가 8이하면
+
+        //5. null 값 또는 빈 문자열일 경우
+        if(s == null || s.isEmpty()) return PasswordStrength.INVALID;
+
+        boolean lengthEnough = s.length() >=8;
+        boolean containsNum = meetsContainingNumberCriteria(s); // 0 ~ 9 라는 숫자를 포함하는지 안하는지 확인하는 변수
+        boolean containsUpp = meetsContainingUppercaseCriteria(s);
+
+
+        //6. 숫자 포함 x and 대문자 포함 x and 8글자 이상만 포함
+        if(lengthEnough && !containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
+        //7.
+        if(!lengthEnough && containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
+        //8.
+        if(!lengthEnough && !containsNum && containsUpp)
+            return PasswordStrength.WEAK;
+
+        /* 이전 코드
+        // 1. 문자열의 길이가 8이하면
+        if (s.length() < 8) {
             return PasswordStrength.NORMAL; // 패스워드의 강도는 보통이다.
         }
-        boolean containsNum = meetsContainingNumberCriteria(s); // 0 ~ 9 라는 숫자를 포함하는지 안하는지 확인하는 변수
-       if(!containsNum) return PasswordStrength.NORMAL; // 각 문자를 비교해서 0 ~ 9 사이의 값을 갖는 문자가 없으면 NORMAL
+         */
+
+        if(!lengthEnough) {
+            return PasswordStrength.NORMAL;
+        }
+        if(!containsNum) return PasswordStrength.NORMAL; // 각 문자를 비교해서 0 ~ 9 사이의 값을 갖는 문자가 없으면 NORMAL
+        if(!containsUpp) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG; // 갖고 있으면 강함으로
     }
 
+    //3. 숫자를 포함하지 않고 나머지 조건을 충족하는 경우
     //숫자(0~9)를 포함하고 있는지 확인하는 로직 -> 분리한다.
     public boolean meetsContainingNumberCriteria(String s) {
         for (char ch : s.toCharArray()) {
             if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean meetsContainingUppercaseCriteria(String s) {
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
                 return true;
             }
         }
--- test/java/chap02/PasswordStrengthMeterTest.java
+++ test/java/chap02/PasswordStrengthMeterTest.java
@@ -6,42 +6,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PasswordStrengthMeterTest {
 
+
+    // 테스트가 잘 작동되는지 확인
     @Test
     void name(){
 
     }
 
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter(); // 공통으로 들어가는 것을 묶어준다.
+
+    //메서드로 분리
+    private void assertStrength(String password, PasswordStrength expStr) {
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
+
+
     // 강력한 암호
      @Test
     void meetsAllCriteria_Then_Strong(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-         PasswordStrength result = meter.meter("2b12!@AB");
-         assertEquals(PasswordStrength.STRONG, result);
-         PasswordStrength result2 = meter.meter("abc1!Add");
-         assertEquals(PasswordStrength.STRONG, result2);
-
-
+         assertStrength("ab12!@AB",PasswordStrength.STRONG);
+         assertStrength("abc1!Add",PasswordStrength.STRONG);
     }
 
     //2. 길이만 8글자 미만이고 나머지 조건은 충적하는 경우
     @Test
     void meetsOtherCriteria_except_for_Length_Then_Normal() {
-        PasswordStrengthMeter meter = new PasswordStrengthMeter(); //객체 만들어준다.
-        PasswordStrength result = meter.meter("ab12!@A");
-        assertEquals(PasswordStrength.NORMAL, result);
-
-        PasswordStrength result2 = meter.meter("Ab12!c");
-        assertEquals(PasswordStrength.NORMAL, result2);
+        assertStrength("ab12!@A",PasswordStrength.NORMAL);
+        assertStrength("Ab12!c", PasswordStrength.NORMAL);
     }
 
 
     //3. 숫자를 포함하지 않고 나머지 조건을 충족하는 경우
     @Test
     void meetsOtherCriteria_excep_for_number_Then_Nomal(){
-        PasswordStrengthMeter meter = new PasswordStrengthMeter();
-        PasswordStrength result = meter.meter("ab!@ABqwer");
-        assertEquals(PasswordStrength.NORMAL, result);
+        assertStrength("ab!@ABqwer",PasswordStrength.NORMAL);
     }
 
+
+    //4.값이 없을 경우  - illegalargumentexception을 발생
+    //               - 유요하지 않은 암호를 의미하는 PasswordStrength.INVALID를 리턴
+    @Test
+    void nullInput_Then_Invalid(){
+        assertStrength(null, PasswordStrength.INVALID);
+    }
+
+    // "" 빈 문자열일 경우
+    @Test
+    void emptyInput_Then_Invalid(){
+        assertStrength("", PasswordStrength.INVALID);
+
+    }
+
+    //5. 대문자를 포함하지 않고 나머지 조건을 충족하는 경우
+    @Test
+    void meetsOtherCriteria_except_for_Uppercase_Then_Normal(){
+        assertStrength("ab12!@df", PasswordStrength.NORMAL);
+    }
+
+    //6. 길이가 8글자 이상인 조건만 충족하는 경우  -> 약함
+    @Test
+    void meetsOnlyLengthCriteria_Then_Weak(){
+        assertStrength("abdefghi", PasswordStrength.WEAK);
+    }
+
+    //7. 숫자 포함 조건만 충족하는 경우
+    @Test
+    void meetsOnlyNumCriteria_Then_Weak(){
+        assertStrength("12345", PasswordStrength.WEAK);
+    }
+
+    //8. 대문자 포함 조건만 충족하는 경우
+    @Test
+    void meetsOnlyUpperCriteria_Then_Weak(){
+        assertStrength("ABCDE", PasswordStrength.WEAK);
+    }
 
 }
